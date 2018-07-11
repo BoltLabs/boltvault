3fb20c3342aafb246a0b4901fe46990233d76343
Alex Rafalovich <alexanderrafalovich@alum.calarts.edu>
2018-07-10 21:04:50 -0700

    Switch websockets to tls

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -25,7 +25,7 @@ export class WebsocketService {
   connect() {
     if (this.socket.connected && this.socket.ws) return;
     delete this.socket.ws; // Maybe this will erase old connections
-    const ws = new WebSocket('ws://peer.boltnode.org:3333');
+    const ws = new WebSocket('wss://peer.boltnode.org:3333');
     this.socket.ws = ws;
 
     ws.onopen = event => {
